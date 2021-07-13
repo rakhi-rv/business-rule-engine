d1ac8c77518184e9d0cd16bdd476741e50f6f5c9
Rakhi Vishwakarma <rakhi.vishwakarma@tradeshift.com>
2021-07-13 10:00:00 +0200

    Initial Commit

---
+++ src/main/java/com/business/rule/engine/businessruleengine/BusinessRuleEngineApplication.java
@@ -0,0 +1,13 @@
+package com.business.rule.engine.businessruleengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRuleEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessRuleEngineApplication.class, args);
+	}
+
+}
